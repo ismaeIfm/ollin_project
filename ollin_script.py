bc000aabaf423c69aa09726b6b73f2d4614af04d
ismaeIfm <ismaelfm_@ciencias.unam.mx>
2015-10-01 19:56:13 -0500

    Fully functional

--- ollin_script.py
+++ ollin_script.py
@@ -1,18 +1,41 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+import sys, getopt, pexpect, time
 
-import subprocess
-import sys
+def main(argv):
+   command = ''
+   master = True
+   try:
+      opts, args = getopt.getopt(argv,"hc:",["command=", "exclude-master"])
+   except getopt.GetoptError:
+      print 'ollin_script.py -c <command>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'ollin_script.py -c <command>'
+         sys.exit()
+      elif opt in ("-c", "--command"):
+         command = arg + ' &'
+      elif opt == "--exclude-master":
+         master = False
 
-#prueba = subprocess.call(["ssh", "root@ollin.fisica.unam.mx", "nohup", "uname -a"])
+   ssh = pexpect.spawn('ssh root@ollin.fisica.unam.mx')
 
+   if master:
+	ssh.sendline(command)
 
-sshProcess = subprocess.Popen(['ssh', "root@ollin.fisica.unam.mx"])
-#sshProcess.stdin.write("uname -a\n")
-#prueba.PIPE.write("ifconfig\n")
+   ssh.sendline('ssh ollin-1 nohup ' + command)
+   ssh.sendline('ssh ollin-2 nohup ' + command)
+   ssh.sendline('ssh ollin-3 nohup ' + command)
+   ssh.sendline('ssh ollin-4 nohup ' + command)
+   ssh.sendline('ssh ollin-5 nohup ' + command)   
+   ssh.sendline('ssh ollin-6 nohup ' + command)
+   ssh.sendline('ssh ollin-7 nohup ' + command)
+   ssh.sendline('ssh ollin-8 nohup ' + command)
+   ssh.sendline('ssh ollin-9 nohup ' + command)
+   ssh.sendline('ssh ollin-10 nohup ' + command)
 
+   time.sleep(1)
 
-#command = sys.argv[1]
-#exclude_master = int(sys.argv[2])
-
-#print "command"
-exit(0)
+	
+if __name__ == "__main__":
+   main(sys.argv[1:])
