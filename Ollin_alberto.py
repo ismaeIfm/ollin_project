5987ba61777cb18b7c6972823f2cf001bcf9631a
Cano Ortiz Alberto <can.ort.alb@gmail.com>
2015-10-02 15:12:02 -0500

    ...

--- Ollin_alberto.py
+++ Ollin_alberto.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python
+
 import pxssh
 
-ollin = pxssh.pxssh()
+#Excluir al master indicando en la segunda entrada de la funcion False.
+#Incluir al master no indicando nada en la sengunda entrada de la funcion.
+ 
+def executeCommand(command,master = True):
 
-command = 'ls -a'
+    ollin = pxssh.pxssh()
 
-if not ollin.login('ollin.fisica.unam.mx','root'):
-    print "ssh session failed longin"
-    print str(ollin)
-else:
-    print "ssh session login successful"
-    master = True
+    if not ollin.login('ollin.fisica.unam.mx','root'):
+        print "ssh session failed longin"
+        print str(ollin)
+    else:
+        print "ssh session login successful"
 
-if master == True:
-    for i in range(1,11):
-        ollin.sendline('ssh root@ollin-'+str(i))
-        ollin.sendline(command)
-        ollin.prompt()
-        print ollin.before
 
-for i in range(1,11):
+        if master == False:
+            for i in range(1,3):
+                ollin.sendline('ssh root@ollin-'+str(i))
+                ollin.sendline(command)
+                ollin.prompt()
+                print ollin.before
+        else:
+            ollin.sendline(command)
+            for i in range(1,3):
+                ollin.sendline('ssh root@ollin-'+str(i))
+                ollin.sendline(command)
+                ollin.prompt()
+                print ollin.before
+                
+
     ollin.logout()
+
+
+
+#executeCommand('ls -a')
+#executeCommand('ls -a',False)
