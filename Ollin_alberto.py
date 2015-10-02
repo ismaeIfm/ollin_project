7f34ac3da0457bad3fb1372a9feff72f0a9cf87d
Cano Ortiz Alberto <can.ort.alb@gmail.com>
2015-10-02 14:10:28 -0500

    ...

--- Ollin_alberto.py
+++ Ollin_alberto.py
@@ -8,18 +8,6 @@ if not ollin.login('ollin.fisica.unam.mx','root'):
     print str(ollin)
 else:
     print "ssh session login successful"
-
-ollin.logout()
-#!/usr/bin/python
-import pxssh
-
-ollin = pxssh.pxssh()
-
-if not ollin.login('ollin.fisica.unam.mx','root'):
-    print "ssh session failed longin"
-    print str(ollin)
-else:
-    print "ssh session login successful"
     master = True
 
 if master == True:
