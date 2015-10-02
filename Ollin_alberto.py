70e9784eafd63343d2c2bb863a85634c1b953712
Cano Ortiz Alberto <can.ort.alb@gmail.com>
2015-10-02 14:29:48 -0500

    ...

--- Ollin_alberto.py
+++ Ollin_alberto.py
@@ -3,6 +3,8 @@ import pxssh
 
 ollin = pxssh.pxssh()
 
+command = 'ls -a'
+
 if not ollin.login('ollin.fisica.unam.mx','root'):
     print "ssh session failed longin"
     print str(ollin)
@@ -13,5 +15,9 @@ else:
 if master == True:
     for i in range(1,11):
         ollin.sendline('ssh root@ollin-'+str(i))
+        ollin.sendline(command)
         ollin.prompt()
         print ollin.before
+
+for i in range(1,11):
+    ollin.logout()
